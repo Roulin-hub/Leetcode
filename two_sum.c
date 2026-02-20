7aa14de8ee65636b03e5a0615e244a154a7831ac
Roulin-hub <roulinvinnarasy@gmail.com>
2026-02-20 10:04:49 +0530

    Add files via upload

---
+++ two_sum.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+int* twoSum(int* nums,int numsSize,int target,int* returnSize)
+{
+	int i,j;
+	int*a=malloc(2*sizeof(int));
+	for(i=0; i<numsSize; i++)
+	{
+		for(j=i+1; j<numsSize; j++)
+		{
+			if(nums[i]+nums[j]==target)
+			{
+				a[0]=i;
+				a[1]=j;
+				*returnSize=2;
+				return a;
+			}
+		}
+	}
+	return a;
+}
