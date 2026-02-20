4eec894ff9f6336c5828760fe3afbb2692035869
Roulin-hub <roulinvinnarasy@gmail.com>
2026-02-20 10:20:27 +0530

    Add files via upload

---
+++ max_subarray.c
@@ -0,0 +1,16 @@
+int maxSubArray(int* nums,int numsSize)
+{
+	int maxSum=nums[0];
+	int curSum=nums[0];
+	int i;
+	for(i=1; i<numsSize; i++)
+	{
+		if(curSum<0)
+			curSum=nums[i];
+		else
+			curSum+=nums[i];
+		if(curSum>maxSum)
+			maxSum=curSum;
+	}
+	return maxSum;
+}
